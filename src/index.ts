a27ecde1df28eaaee0099451085b05559efb4592
mayne <i@@gine.me>
2023-12-17 22:58:10 +0800

    feat: api for checking file exist

--- src/index.ts
+++ src/index.ts
@@ -43,10 +43,24 @@ app.post('/api/register', async (c) => {
 	}
 });
 
+// check file is exist
+app.get('/:subdomain/check-file/:path{.*}', async (c) => {
+	const path = c.req.param('path');
+	const subdomain = c.req.param('subdomain');
+	const key = `${subdomain}/${path}`;
+	const object = await c.env.EIDOS_PUBLISH_BUCKET.get(key);
+	if (object === null) {
+		return c.json({ success: false, message: 'File not found' });
+	}
+	return c.json({ success: true });
+});
+
 // get file
 app.get('/:subdomain/files/:path{.*}', async (c) => {
 	const path = c.req.param('path');
-	const object = await c.env.EIDOS_PUBLISH_BUCKET.get(path);
+	const subdomain = c.req.param('subdomain');
+	const key = `${subdomain}/${path}`;
+	const object = await c.env.EIDOS_PUBLISH_BUCKET.get(key);
 	if (object === null) {
 		return c.text('Not Found', {
 			status: 404,
@@ -120,7 +134,8 @@ app.put('/:subdomain/files/:path{.*}', async (c) => {
 	if (!valid) {
 		return c.json({ success: false, message: 'authentication failed' });
 	}
-	await c.env.EIDOS_PUBLISH_BUCKET.put(path, c.req.raw.body);
+	const key = `${subdomain}/${path}`;
+	await c.env.EIDOS_PUBLISH_BUCKET.put(key, c.req.raw.body);
 	return c.json({ success: true });
 });
 // ykyn3mndvhqe7sp8grb0d
